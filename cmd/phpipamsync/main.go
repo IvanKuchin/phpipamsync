9f8b621a997a68ec987e81414936e17d0921d9fa
Ivan Kuchin (ikuchin) <ivan.kuchin@gmail.com>
2023-10-24 19:52:41 -0400

    main app moved to cmd-folder

---
+++ cmd/phpipamsync/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"github.com/ivankuchin/phpipamsync/internal/cli"
+	"github.com/ivankuchin/phpipamsync/internal/config_reader"
+)
+
+func SetLogFlags() {
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
+}
+
+func main() {
+	SetLogFlags()
+
+	_, err := config_reader.Read()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	cli.Execute()
+}
